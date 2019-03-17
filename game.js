5a80c55f5479916aa3e7105bdfaa4753476ba2ea
Mauro Falcone <maurofalcone@Mauro-Falcone.local>
2019-03-17 19:00:55 -0300

    enabled to change the direction

--- game.js
+++ game.js
@@ -2,7 +2,12 @@ var canvas = null
 var ctx = null
 var x = 0
 var y = 0
-var lastPress = null 
+var lastPress = null
+var KEY_LEFT = 37,
+    KEY_UP = 38,
+    KEY_RIGHT = 39,
+    KEY_DOWN = 40
+var dir = 0
 
 window.requestAnimationFrame = ( function () {
     return window.requestAnimationFrame ||
@@ -13,18 +18,58 @@ window.requestAnimationFrame = ( function () {
         }
 }())
 
+
 function act() {
-    x += 2
-    if( x > canvas.width)
+    // Change Direction
+    if (lastPress == KEY_UP) {
+        dir = 0
+    }
+    if (lastPress == KEY_RIGHT) {
+        dir = 1
+    }
+    if (lastPress == KEY_DOWN) {
+        dir = 2
+    }
+    if (lastPress == KEY_LEFT) {
+        dir = 3
+    }
+
+    // Move Rectangle
+    if(dir == 0) {
+        y += 10
+    }
+    if(dir == 1) {
+        x += 10
+    }
+    if(dir == 2) {
+        y -= 10
+    }
+    if(dir == 3) {
+        x -= 10
+    }
+
+    // Out Screen
+    if (x > canvas.width) {
         x = 0
-    if( y > canvas.height)
+    }
+    if (y > canvas.height) {
         y = 0
+    }
+    if (x < 0) {
+        x = canvas.width
+    }
+    if (y < 0) {
+        y = canvas.height
+}
+
+function repaint() {
+    window.requestAnimationFrame(repaint)
+    paint(ctx)
 }
 
 function run() {
-    window.requestAnimationFrame(run)
+    window.setTimeout(50)
     act()
-    paint(ctx)
 }
 
 function paint(ctx) {
@@ -36,6 +81,8 @@ function paint(ctx) {
     // rectangle position and size
     //           x   y  width height
     ctx.fillRect(x, y, 10, 6)
+
+    ctx.fillText('Last Press: ' + lastPress, 0, 20)
 }
 
 function init() {
