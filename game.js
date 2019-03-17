1eda5efc26894cb28c24459ec035863bb41e7918
Mauro Falcone <maurofalcone@Mauro-Falcone.local>
2019-03-17 18:30:36 -0300

    clear canvas and new border

--- game.js
+++ game.js
@@ -1,7 +1,13 @@
 var canvas = null
 var ctx = null
+var x = 50
+var y = 50
 
 function paint(ctx) {
+    //clear the canvas
+    ctx.fillStyle = '#FFF'
+    ctx.fillRect(0, 0, canvas.width, canvas.height)
+
     ctx.fillStyle = '#0F0'
     // rectangle position and size
     //           x   y  width height
